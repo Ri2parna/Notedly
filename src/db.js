7785f905119759c02ef1d780a997831d0336f64e
Rituparna Das <ri2parna@outlook.com>
2024-02-08 19:35:46 +0000

    fix: updating packages

--- src/db.js
+++ src/db.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 module.exports = {
     connect: DB_HOST => {
         // use updated URL string parser
-        mongoose.set('useFindAndModify', false);
-        mongoose.set('useCreateIndex', true);
-        mongoose.set('useUnifiedTopology', true);
         mongoose.connect(DB_HOST, { useNewUrlParser: true });
         mongoose.connection.on('error', err => {
             console.log(err);
